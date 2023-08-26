a9bc3d4be33b601da05fdfe8828828e7b553762f
ISMAIL PALALI <ismail.palali.pp@gmail.com>
2023-08-26 21:55:59 +0300

    add network manager and service constant

---
+++ NewsApp/Utilities/Network/Constant/ServiceConstant/ServiceConstant.swift
@@ -0,0 +1,26 @@
+//
+//  ServiceConstant.swift
+//  NewsApp
+//
+//  Created by İsmail Palalı on 26.08.2023.
+//
+
+import Foundation
+
+extension Constant {
+
+    class Network {
+
+        enum ServiceEndPoint: String {
+
+            case BASE_URL = "https://newsapi.org/v2/top-headlines/sources?"
+            case API_KEY = "apiKey=8e9f2552595e46968c630269fe0eda9"
+
+            //https://newsapi.org/v2/top-headlines/sources?apiKey=8e9f2552595e46968c630269fe0eda98
+            
+            static func fetchArticles() -> String {
+                "\(ServiceEndPoint.BASE_URL.rawValue)\(ServiceEndPoint.API_KEY.rawValue)"
+            }
+        }
+    }
+}
