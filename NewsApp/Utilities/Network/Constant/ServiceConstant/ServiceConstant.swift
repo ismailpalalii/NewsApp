fb69ee9e94c094e0aadc1c389fd8da7483a2a071
ISMAIL PALALI <ismail.palali.pp@gmail.com>
2023-08-26 23:09:11 +0300

    add newsDetail and newscategory service and models

--- NewsApp/Utilities/Network/Constant/ServiceConstant/ServiceConstant.swift
+++ NewsApp/Utilities/Network/Constant/ServiceConstant/ServiceConstant.swift
@@ -13,14 +13,24 @@ extension Constant {
     class Network {
 
         enum ServiceEndPoint: String {
+            case baseUrl = "https://newsapi.org/v2/top-headlines/sources?"
+            case baseDetailUrl = "https://newsapi.org/v2/top-headlines?sources"
+            case baseCategoryUrl = "https://newsapi.org/v2/top-headlines/sources?category"
+            case apikey = "apiKey=8e9f2552595e46968c630269fe0eda98"
 
-            case BASE_URL = "https://newsapi.org/v2/top-headlines/sources?"
-            case API_KEY = "apiKey=8e9f2552595e46968c630269fe0eda9"
-
+            //Check url path
             //https://newsapi.org/v2/top-headlines/sources?apiKey=8e9f2552595e46968c630269fe0eda98
+            //https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=8e9f2552595e46968c630269fe0eda98
+            //https://newsapi.org/v2/top-headlines/sources?category=businessapiKey=8e9f2552595e46968c630269fe0eda98
             
             static func fetchNewsPath() -> String {
-                "\(ServiceEndPoint.BASE_URL.rawValue)\(ServiceEndPoint.API_KEY.rawValue)"
+                "\(ServiceEndPoint.baseUrl.rawValue)\(ServiceEndPoint.apikey.rawValue)"
+            }
+            static func fetchNewsDetailPath(id: String) -> String {
+                "\(ServiceEndPoint.baseDetailUrl.rawValue)\("=\(id)&")\(ServiceEndPoint.apikey.rawValue)"
+            }
+            static func fetchNewsCategoryPath(category: String) -> String {
+                "\(ServiceEndPoint.baseCategoryUrl.rawValue)\("=\(category)&")\(ServiceEndPoint.apikey.rawValue)"
             }
         }
     }
--- NewsApp/Utilities/Network/Service/NetworkService.swift
+++ NewsApp/Utilities/Network/Service/NetworkService.swift
@@ -10,10 +10,27 @@ import Alamofire
 // MARK: - ServiceProtocol
 protocol NetworkServiceProtocol: AnyObject {
     func fetchNews(onSuccess: @escaping (NewsModel?) -> Void, onError: @escaping (AFError) -> Void)
+    func fetchNewsDetail(id: String, onSuccess: @escaping (NewsDetail?) -> Void, onError: @escaping (AFError) -> Void)
+    func fetchNewsCategory(category: String, onSuccess: @escaping (NewsModel?) -> Void, onError: @escaping (AFError) -> Void)
 }
 
 // MARK: - Services
 final class NetworkService: NetworkServiceProtocol {
+    func fetchNewsCategory(category: String, onSuccess: @escaping (NewsModel?) -> Void, onError: @escaping (Alamofire.AFError) -> Void) {
+        NetworkManager.shared.fetch(path: Constant.Network.ServiceEndPoint.fetchNewsCategoryPath(category: category)) { (response: NewsModel) in
+            onSuccess(response)
+        } onError: { error in
+            onError(error)
+        }
+    }
+
+    func fetchNewsDetail(id: String, onSuccess: @escaping (NewsDetail?) -> Void, onError: @escaping (Alamofire.AFError) -> Void) {
+        NetworkManager.shared.fetch(path: Constant.Network.ServiceEndPoint.fetchNewsDetailPath(id: id)) { (response: NewsDetail) in
+            onSuccess(response)
+        } onError: { error in
+            onError(error)
+        }
+    }
 
     func fetchNews(onSuccess: @escaping (NewsModel?) -> Void, onError: @escaping (AFError) -> Void) {
         NetworkManager.shared.fetch(path: Constant.Network.ServiceEndPoint.fetchNewsPath()) { (response: NewsModel) in
