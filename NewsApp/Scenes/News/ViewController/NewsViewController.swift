5532384e0836dff0c92420849461f3c470e77a00
ISMAIL PALALI <ismail.palali.pp@gmail.com>
2023-08-27 11:59:38 +0300

    add NewsViewController and xib

---
+++ NewsApp/Scenes/News/ViewController/NewsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NewsViewController.swift
+//  NewsApp
+//
+//  Created by İsmail Palalı on 27.08.2023.
+//
+
+import UIKit
+
+final class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
