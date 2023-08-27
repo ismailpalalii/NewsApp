6818b0e9f3777a6131d6418a1c40c15d65a3e2b8
ISMAIL PALALI <ismail.palali.pp@gmail.com>
2023-08-27 12:29:11 +0300

    delete xib and add  categoryCollectionView & newsSourceTableView

--- NewsApp/Scenes/News/ViewController/NewsViewController.swift
+++ NewsApp/Scenes/News/ViewController/NewsViewController.swift
@@ -6,11 +6,69 @@
 //
 
 import UIKit
+import SnapKit
 
 final class NewsViewController: UIViewController {
 
+    // MARK: Create UI items
+    private lazy var categoryCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let collectionView = UICollectionView(frame: .zero,
+                                              collectionViewLayout: layout)
+        collectionView.backgroundColor = .white
+        return collectionView
+    }()
+
+    private lazy var newsSourcesTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.rowHeight = 80
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
+    }
 
+    // MARK: - UI Configure
+    private func configure() {
+        title = "Kaynaklar"
+        view.backgroundColor = .systemBackground
+        view.addSubview(categoryCollectionView)
+        view.addSubview(newsSourcesTableView)
+
+        newsSourcesTableView.delegate = self
+        newsSourcesTableView.dataSource = self
+        categoryCollectionView.delegate = self
+        categoryCollectionView.dataSource = self
+
+        navigationItem.hidesBackButton = true
+
+
+        // MARK: Constraints
+
+
+    }
+}
+
+extension NewsViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension NewsViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
     }
 }
