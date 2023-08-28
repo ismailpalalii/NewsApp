e49da5a4b8ffa041d67da2bb4b67aa67b4367896
ISMAIL PALALI <ismail.palali.pp@gmail.com>
2023-08-28 22:57:00 +0300

    fix BaseViewController life cycle

--- NewsApp/Utilities/Common/BaseViewController.swift
+++ NewsApp/Utilities/Common/BaseViewController.swift
@@ -9,10 +9,29 @@ import UIKit
 
 class BaseViewController: UIViewController, BaseViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("lifeCycleInfo viewWillAppear - \(Self.className)")
+    }
 
-        print("lifeCycleInfo viewDidLoad - \(Self.className)")
+    override func viewDidLoad() {
+       super.viewDidLoad()
+       print("lifeCycleInfo viewDidLoad - \(Self.className)")
+   }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("lifeCycleInfo viewDidAppear - \(Self.className)")
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("lifeCycleInfo viewWillDisappear - \(Self.className)")
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("lifeCycleInfo viewDidDisappear - \(Self.className)")
     }
 }
 
--- NewsApp/Utilities/Network/Service/NetworkService.swift
+++ NewsApp/Utilities/Network/Service/NetworkService.swift
@@ -10,8 +10,6 @@ import Alamofire
 // MARK: - ServiceProtocol
 protocol NetworkServiceProtocol: AnyObject {
     func fetchNews(completion: @escaping (NewsModel?) -> Void)
-    //func fetchNewsDetail(id: String, completion: @escaping (NewsDetail?) -> Void)
-    //func fetchNewsCategory(category: String, completion: @escaping (NewsModel?) -> Void)
 }
 
 // MARK: - Services
@@ -26,32 +24,4 @@ final class NetworkService: NetworkServiceProtocol {
             }
         }
     }
-
-//    func fetchNewsDetail(id: String, completion: @escaping (NewsDetail?) -> Void) {
-//        <#code#>
-//    }
-//
-//    func fetchNewsCategory(category: String, completion: @escaping (NewsModel?) -> Void) {
-//        <#code#>
-//    }
-
-//    func fetchNewsCategory(category: String, onSuccess: @escaping (NewsModel?) -> Void, onError: @escaping (Alamofire.AFError) -> Void) {
-//
-//    }
-//
-//    func fetchNewsDetail(id: String, onSuccess: @escaping (NewsDetail?) -> Void, onError: @escaping (Alamofire.AFError) -> Void) {
-//        NetworkManager.shared.fetch(path: Constant.Network.ServiceEndPoint.fetchNewsDetailPath(id: id)) { (response: NewsDetail) in
-//            onSuccess(response)
-//        } onError: { error in
-//            onError(error)
-//        }
-//    }
-//
-//    func fetchNews(onSuccess: @escaping (NewsModel?) -> Void, onError: @escaping (AFError) -> Void) {
-//        NetworkManager.shared.fetch(path: Constant.Network.ServiceEndPoint.fetchNewsPath()) { (response: NewsModel) in
-//            onSuccess(response)
-//        } onError: { error in
-//            onError(error)
-//        }
-//    }
 }
