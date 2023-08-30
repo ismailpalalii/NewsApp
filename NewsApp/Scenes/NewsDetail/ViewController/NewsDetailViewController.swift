48e5fd5cc16f74f1bad30af4681fbb87e196383e
ISMAIL PALALI <ismail.palali.pp@gmail.com>
2023-08-30 13:50:15 +0300

    add News Detail Screen UI items and News Detail View Model

--- NewsApp/Scenes/NewsDetail/ViewController/NewsDetailViewController.swift
+++ NewsApp/Scenes/NewsDetail/ViewController/NewsDetailViewController.swift
@@ -9,8 +9,38 @@ import UIKit
 
 final class NewsDetailViewController: BaseViewController {
     // MARK: Create UI items
-     var sourceTitle: String?
-     var sourceID: String?
+    private lazy var sliderCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero,
+                                              collectionViewLayout: layout)
+        collectionView.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        return collectionView
+    }()
+
+    private lazy var newsListCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let collectionView = UICollectionView(frame: .zero,
+                                              collectionViewLayout: layout)
+        collectionView.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        return collectionView
+    }()
+
+
+    private lazy var activityIndicator: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.large)
+        indicator.hidesWhenStopped = true
+        return indicator
+    }()
+
+    private let refreshControl = UIRefreshControl()
+
+    var sourceTitle: String?
+    var sourceID: String?
+
+    // MARK: ViewModel
+    private var viewModel = NewsDetailViewModel()
 
     init(sourceTitle: String? = nil, sourceID: String? = nil) {
         self.sourceTitle = sourceTitle
@@ -29,5 +59,36 @@ final class NewsDetailViewController: BaseViewController {
     private func configure() {
         title = sourceTitle
         view.backgroundColor = .systemBackground
+        view.addSubview(sliderCollectionView)
+        view.addSubview(newsListCollectionView)
+        view.addSubview(activityIndicator)
+
+        newsListCollectionView.refreshControl = refreshControl
+        refreshControl.addTarget(self, action: #selector(refreshData), for: .valueChanged)
+
+        navigationItem.hidesBackButton = true
+
+        // MARK: Constraints
+        sliderCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(16)
+            make.height.equalTo(100)
+            make.left.equalToSuperview().offset(8)
+            make.right.equalToSuperview().offset(-8)
+        }
+
+        newsListCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(sliderCollectionView.snp.bottom).offset(32)
+            make.bottom.equalToSuperview().offset(8)
+            make.width.equalToSuperview()
+        }
+
+        activityIndicator.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
     }
-}
+
+    // MARK: Pull to refresh source list data
+    @objc private func refreshData() {
+
+        }
+    }
---
+++ NewsApp/Scenes/NewsDetail/ViewModel/NewsDetailViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  NewsDetailViewModel.swift
+//  NewsApp
+//
+//  Created by İsmail Palalı on 30.08.2023.
+//
+
+import Foundation
+
+final class NewsDetailViewModel {}
