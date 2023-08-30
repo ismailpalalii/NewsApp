4c8fa4adb3d1431bbd27856a164382c8ea3d9439
ISMAIL PALALI <ismail.palali.pp@gmail.com>
2023-08-30 12:57:43 +0300

    add NewsDetailControlller

---
+++ NewsApp/Scenes/NewsDetail/ViewController/NewsDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NewsDetailViewController.swift
+//  NewsApp
+//
+//  Created by İsmail Palalı on 30.08.2023.
+//
+
+import UIKit
+
+final class NewsDetailViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
